6e6997b2c3498f62bb333d746b8840026ebc8ab2
Vikram Ohri <VikramOhri9@gmail.com>
2024-10-20 15:46:53 +0530

    Connecting UI and Server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'BookJournal_UI';
+
+  private apiUrl = '/server'; // Note: The base URL should match the path you defined in proxy.conf.json
+
+  constructor(private http: HttpClient) { }
+
+  response = "";
+
+  getTestData() {
+    return this.http.get(`${this.apiUrl}/test`, { responseType: 'text' }); // This request will be proxied
+  }
+
+  ngOnInit() {
+    this.getTestData().subscribe((res) => {
+      this.response = res;
+    },
+    (error) => {
+      console.error('Error', error);
+    })
+  }
 }
